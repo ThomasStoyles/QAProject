4e23de72e9e7b571fc8b6cbf9ddc2fa61769a66d
Thomas Stoyles <thomasstoyles1@gmail.com>
2022-06-01 15:06:21 +0000

    TS: Database updates and removal of unneeded code

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'SECRET'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 db = SQLAlchemy(app)
 
