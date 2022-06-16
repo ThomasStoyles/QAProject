d2b6c873b89daf16bbf9ae64b151c4359f9c3bb2
Thomas Stoyles <thomasstoyles1@gmail.com>
2022-06-16 14:03:13 +0000

    TS:Tests

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'SECRET'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 db = SQLAlchemy(app)
