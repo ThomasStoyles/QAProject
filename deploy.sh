2bc3a2ce11b11b1a38cdc4c57271cca7ff24b7dc
Thomas Stoyles <thomasstoyles1@gmail.com>
2022-06-16 13:16:25 +0000

    TS:Adding Deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt install python3 python3-pip python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+echo "killing existing processes"
+sudo kill $(cat gunicornpidfile)
+python3 create.py
+python3 -m gunicorn -D -b  0.0.0.0:5000 -w 4 application:app  -p gunicornpidfile
